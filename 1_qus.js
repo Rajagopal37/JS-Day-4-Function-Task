d91296414fb2d6ddf88fba910377c928f6924cb4
Rajagopal37 <trajagopal37@gmail.com>
2024-03-12 18:31:46 +0530

    1st done

--- 1_qus.js
+++ 1_qus.js
@@ -6,44 +6,44 @@
 //Arrow Function    : Syntax: var varName = (parameter)=>{};      varName();
 
         //anonymous function
-        let odd = function(a) {
-            arr =[];
-            for ( let i=0; i<=a.length; i++) {
-                if (i%2==1) {
-                    arr.push(i);
+            let odd = function(a) {
+                arr =[];
+                for ( let i=0; i<=a.length; i++) {
+                    if (i%2==1) {
+                        arr.push(i);
+                    }
                 }
+                console.log(`1.anonymous: 1,2,3,4,5 -in Odd numbers are ${arr}`,arr)
+                return arr
             }
-            console.log(`1.anonymous: 1,2,3,4,5 -in Odd numbers are ${arr}`,arr)
-            return arr
-        }
 
-        odd([1,2,3,4,5]);
+            odd([1,2,3,4,5]);
         //--------------------------------------------
 
         //IIFE
-        (   function(a){
-            let arr =[];
-            for ( let i=0; i<=a.length; i++) {
-                if (i%2==1) { arr.push(i); }
-            }
-            console.log(`1.IIFE:      1,2,3,4,5 -in odd numbers are ${arr}`, arr)
-            // console.log(arr)
-            return arr
-            }
-        )   ([1,2,3,4,5]);
+            (   function(a){
+                let arr =[];
+                for ( let i=0; i<=a.length; i++) {
+                    if (i%2==1) { arr.push(i); }
+                }
+                console.log(`1.IIFE:      1,2,3,4,5 -in odd numbers are ${arr}`, arr)
+                // console.log(arr)
+                return arr
+                }
+            )   ([1,2,3,4,5]);
         //--------------------------------------------
 
         //Arrow Function
-        let oddNum = (a) => { 
-            let arr =[];
-            for ( let i=0; i<=a.length; i++) {
-                if (i%2==1) { arr.push(i); }
-            }
-            console.log(`1.Arrow:     1,2,3,4,5 -in odd numbers are ${arr}`, arr)
-            // console.log(arr)
-            return arr
-            }
-        oddNum([1,2,3,4,5])
+            let oddNum = (a) => { 
+                let arr =[];
+                for ( let i=0; i<=a.length; i++) {
+                    if (i%2==1) { arr.push(i); }
+                }
+                console.log(`1.Arrow:     1,2,3,4,5 -in odd numbers are ${arr}`, arr)
+                // console.log(arr)
+                return arr
+                }
+            oddNum([1,2,3,4,5])
 
 
 //-----------------------------------------------------------------
