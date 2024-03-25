4e6d3a288d8ed00052bcdc9643267b65e2ce8db4
Rajagopal37 <trajagopal37@gmail.com>
2024-03-25 19:24:41 +0530

    plmdrme

--- 4_qus.js
+++ 4_qus.js
@@ -1,3 +1,8 @@
+//                         Return all the prime numbers in an array
+
 //Anonymos Function : Syntax: var funName = function(){return};   funName();
 //IIFE Function     : Syntax: (function FunctionName () {} ) ( );
 //Arrow Function    : Syntax: var varName = (parameter)=>{};      varName();
+
+
+let 
--- 5_qus.js
+++ 5_qus.js
@@ -1,3 +1,15 @@
+//                   Return all the palindromes in an array
+
 //Anonymos Function : Syntax: var funName = function(){return};   funName();
 //IIFE Function     : Syntax: (function FunctionName () {} ) ( );
 //Arrow Function    : Syntax: var varName = (parameter)=>{};      varName();
+
+
+var arr = ["abc","did","Mom","Dad","xyz",300];
+
+function isPalindrome (str) {  
+    let right = str.toLowerCase().replace(/[^a-z0-9]/g,"");
+    let left = right.split("").reverse().join("");
+    return right === left ;
+}
+
